7257742942cf802597693681e33715b310850a75
Olena <anyanyme@gmail.com>
2023-01-28 19:29:07 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngridients = document.querySelector(`#ingredients`);
+// console.log(listIngridients);
+
+const listItems = ingredients.map((ingredient) => { 
+
+  const listEl = document.createElement(`li`);
+  listEl.classList.add(`item`);
+  
+  
+  const listElText = document.createElement(`p`);
+  listElText.textContent = ingredient;
+listEl.appendChild(listElText);
+ 
+  return listEl;
+ 
+});
+
+// console.log(listItems);
+
+listIngridients.append(...listItems);
+
+
