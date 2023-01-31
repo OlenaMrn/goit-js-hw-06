aabbe1207a582726bf2b53dc7ccf5f5c33cf075b
Olena <anyanyme@gmail.com>
2023-01-31 13:03:39 +0100

    update task 05

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const input = document.querySelector(`#name-input`);
+const nameOutput = document.querySelector(`#name-output`);
+ 
+console.log(input);
+
+input.addEventListener(`input`, onInputSubmit);
+
+function onInputSubmit() {
+   input.value === "" ? nameOutput.textContent = `Anonymous` : nameOutput.textContent = input.value;
+};
