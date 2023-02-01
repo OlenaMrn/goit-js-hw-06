b458a44cd0d54be7324b64ac435f14a1c8002bb3
Olena <anyanyme@gmail.com>
2023-02-01 15:07:53 +0100

    update task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(`.login-form`);
 
+form.addEventListener(`submit`, onFormSubmit);
+
+function onFormSubmit(event) {
+event.preventDefault();
+
+
+const formData = new FormData(event.currentTarget);
+    
+ 
+    
+ formData.forEach((value, name) => {
+        if (value !== "") {
+             console.log(name, value);           
+           
+        } else {alert("Всі поля повинні бути заповнені"); }
+       
+ });
+    
+    
+};
