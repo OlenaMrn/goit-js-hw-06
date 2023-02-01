94e274aa5ebb649d60dea4a5b708963ec1f1e3f3
Olena <anyanyme@gmail.com>
2023-02-01 17:54:23 +0100

    update task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
+const body = document.querySelector(`body`);
+
+const button = document.querySelector(`.change-color`);
+
+const colorSpan = document.querySelector(`.color`);
+
+button.addEventListener(`click`, onColorChange);
+
+function onColorChange() {
+  body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`;
+
+  colorSpan.textContent = `#${Math.floor(Math.random() * 16777215).toString(
+    16
+  )}`;
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
