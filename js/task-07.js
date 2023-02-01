a2afc0c31d19d171eefd20fa3a01ce9223dc8091
Olena <anyanyme@gmail.com>
2023-02-01 10:57:35 +0100

    update task-07

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector(`#font-size-control`);
+const spanEl = document.querySelector(`#text`)
+
+
+inputEl.addEventListener(`input`, onInputChange);
+
+function onInputChange() { 
+    spanEl.style.fontSize = `${event.currentTarget.value}px`;
+   
+};
