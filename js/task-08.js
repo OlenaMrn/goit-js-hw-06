68d5627b5f5349fd97728aa38385f33ba048c49c
Olena <anyanyme@gmail.com>
2023-02-14 10:13:42 +0100

    correct task 08

--- js/task-08.js
+++ js/task-08.js
@@ -3,20 +3,22 @@ const form = document.querySelector(`.login-form`);
 form.addEventListener(`submit`, onFormSubmit);
 
 function onFormSubmit(event) {
-event.preventDefault();
+     event.preventDefault();
+         
+     const formData = new FormData(event.currentTarget);
+     
 
+     if (Array.from(formData.values()).some((value) => value === "")) {
+           alert("Всі поля повинні бути заповнені");
+          return;
+      }
 
-const formData = new FormData(event.currentTarget);
-    
- 
-    
- formData.forEach((value, name) => {
-        if (value !== "") {
-             console.log(name, value);           
-           
-        } else {alert("Всі поля повинні бути заповнені"); }
+     formData.forEach((value, name) => {
+          console.log(name, value);
        
- });
-    
-    event.target.reset(); 
-};
+          event.target.reset();
+     });
+}
+
+
+
