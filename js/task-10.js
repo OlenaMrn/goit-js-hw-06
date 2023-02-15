0d52002f21435e8ad650bf1ba85554aeea9a6c83
Olena <anyanyme@gmail.com>
2023-02-15 10:07:09 +0100

    update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,9 +1,7 @@
-
 const boxes = document.querySelector(`#boxes`);
 const buttonCreate = document.querySelector("button[data-create]");
 const buttonDestroy = document.querySelector("button[data-destroy]");
 const input = document.querySelector(`input`);
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -11,30 +9,29 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-
-buttonCreate.addEventListener(`click`, function(event) {
+buttonCreate.addEventListener(`click`, function (event) {
   createBoxes(event);
 });
 
-function createBoxes(event) { 
+function createBoxes(event) {
   const amount = input.valueAsNumber;
+  const allBoxes = [];
 
   for (let i = 1; i <= amount; i++) {
     const box = document.createElement(`div`);
     box.style.width = `${30 + 10 * (i - 1)}px`;
     box.style.height = `${30 + 10 * (i - 1)}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxes.appendChild(box);
+
+    allBoxes.push(box);
   }
-};
+  //  console.log(allBoxes);
+  boxes.append(...allBoxes);
+  // console.log(boxes);
+}
 
 buttonDestroy.addEventListener(`click`, destroyBoxes);
 
 function destroyBoxes() {
-  while (boxes.firstChild) {
-  boxes.removeChild(boxes.firstChild);
- }
-};
-  
-
-
+  boxes.innerHTML = "";
+}
